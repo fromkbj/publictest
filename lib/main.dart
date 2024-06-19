ed3f3d61f1f872d856ea2566d39e096dfa4bc0e9
test <kbjmay19@gmail.com>
2024-06-19 17:04:14 +0900

    feat: change title of app

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'AnD Todo app',
       theme: ThemeData(
         // This is the theme of your application.
         //
